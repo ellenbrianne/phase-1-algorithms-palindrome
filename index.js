33dc4eb81bdf33339e7b6bdd431f0656903deb51
Ellen Bennett <ellenbennett@Ellens-MacBook-Air.local>
2023-03-27 23:33:03 -0600

    passing lab, working palindrome tester function

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let characters = word.split("")
+  let reverse = []
+  
+  for (let i = characters.length - 1; i >= 0; i--) {
+    const element = characters[i];
+    reverse.push(element)
+  }
+
+  if (characters.join("") == reverse.join("")) {
+    return true
+  } else {
+    return false
+  }
 }
 
+console.log(isPalindrome("madam"))
 /* 
-  Add your pseudocode here
+  function must first separate the passed 'word' into each individual character of the 'word', stored in a new array
+  then iterate through that array but backwards
+  and only return true if backwards === forwards
 */
 
 /*
-  Add written explanation of your solution here
+  used built-in .split() method to separate each character of passed in word and stored that to its own array
+  used a for loop to iterate through the characters array from the last index, backward, and end at the first index
+  created an empty array to store each element from the for loop
+  used the built-in .push() method to automatically store each individual element into the reverse array, as it was being iterated over
+  lastly, created a conditional that stated if the original array matched the reversed array to return true & false if not; however, I had to re-join all of the individual characters in each of those arrays so i could compare each array as strings, so i used the .join() method inside of the conditional
 */
 
 // You can run `node index.js` to view these console logs
